e0ed9daa6c7c58178319101819c3517ea9101f74
finchyx <68973829+finchyx@users.noreply.github.com>
2020-07-29 21:18:20 -0400

    Add files via upload

--- Method2.java
+++ Method2.java
@@ -1,18 +1,23 @@
 
-public class example1 {
-    public example1() {
+public class Method2 {
+    public Method2() {
         {
             for (int i = 0; i < 5; i++) {
                 for (int j = 4; j > i; j--) {
                     System.out.print(".");
                 }
                 int count = i + 1;
-                System.out.print("" + count);
-                System.out.println("");
+                System.out.print(count);
+
+                for (int c = 0; c < i; c++) {
+                    System.out.print(count);
+                }
+                System.out.println();
             }
         }
     }
+
     public static void main(String[] args) {
-        new example1();
+        new Method2();
     }
 }
